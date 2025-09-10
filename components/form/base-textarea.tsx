2fca73f113a105e8f25c45bf8b5c8094859cd01d
haseebbsal1 <haseeb.salman1234@outlook.com>
2025-09-10 05:12:58 +0500

    client prod changes

--- components/form/base-textarea.tsx
+++ components/form/base-textarea.tsx
@@ -1,14 +1,36 @@
-'use client'
+"use client";
 
-import { BaseTextAreaProps } from "@/utils/types"
-import { Textarea } from "@nextui-org/react"
-import { Control, useController } from "react-hook-form"
+import { BaseTextAreaProps } from "@/utils/types";
+import { Textarea } from "@nextui-org/react";
+import { Control, useController } from "react-hook-form";
 
-export default function BaseTextArea({control,name,rules,label,labelPlacement,placeholder,minRows=20,variant="bordered"}:BaseTextAreaProps) {
-    const {field,fieldState:{error}}=useController({control,name,rules})
-    return (
-        <>
-            <Textarea  {...field} isInvalid={!!error} errorMessage={error?.message} classNames={{ label: 'font-semibold' }} label={label} labelPlacement={labelPlacement} placeholder={placeholder} minRows={minRows} variant={variant} />
-        </>
-    )
+export default function BaseTextArea({
+  control,
+  name,
+  rules,
+  label,
+  labelPlacement,
+  placeholder,
+  minRows = 20,
+  variant = "bordered",
+}: BaseTextAreaProps) {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({ control, name, rules });
+  return (
+    <>
+      <Textarea
+        {...field}
+        isInvalid={!!error}
+        errorMessage={error?.message}
+        classNames={{ label: "font-semibold !text-black", input: "!text-black" }}
+        label={label}
+        labelPlacement={labelPlacement}
+        placeholder={placeholder}
+        minRows={minRows}
+        variant={variant}
+      />
+    </>
+  );
 }
